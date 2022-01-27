50c6924592beda36662e93f179879cd7cd58d316
aakirahin <nihsunwar@gmail.com>
2022-01-27 12:05:56 +0000

    reviewed and style nav bar

--- lib/reusables/navbar/nav.dart
+++ lib/reusables/navbar/nav.dart
@@ -1,73 +1,80 @@
 import 'package:flutter/material.dart';
 
-
 class Nav extends StatefulWidget {
+  const Nav({Key? key}) : super(key: key);
+
   @override
   _NavState createState() => _NavState();
 }
 
 class _NavState extends State<Nav> {
-  int _selectedIndex = 0;
-  List<Widget>_widgetOptions = <Widget>[
-    Text('Map'), //Map(),
-    Text('Profile'), //Profile()
-    Text('About Us'), //AboutUs()
-  ]; 
-
-  void _onItemTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title: Text("Bottom Nav Bar"),
-        ), //App Bar
-        body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.map),
-              label: "Map"),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Profile"),  
-              
-            BottomNavigationBarItem(
-              icon: Icon(Icons.star),
-              label: "About Us"),
-
-          ],
-     currentIndex: _selectedIndex,
-     onTap: _onItemTap,
-      ), //BottomNavigationBar
-      ); //Scaffold
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            child: IconButton(
+              iconSize: 70,
+              onPressed: () { 
+                // Go to map page
+              }, 
+              icon: Image.asset("assets/images/map-icon.png"),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(1, 1),
+                )
+              ]
+            )
+          ),
+          Container(
+            child: IconButton(
+              iconSize: 70,
+              onPressed: () {
+                // Go to profile page
+              }, 
+              icon: Image.asset("assets/images/profile-icon.png"),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(1, 1),
+                )
+              ]
+            )
+          ),
+          Container(
+            child: IconButton(
+              iconSize: 70,
+              onPressed: () {
+                // Go to about us page
+              }, 
+              icon: Image.asset("assets/images/aboutus-icon.png"),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(1, 1),
+                )
+              ]
+            )
+          ),
+        ],
+      )
+    );
   }
 }
-
-
-// This part should be added to main.dart to run this navbar. ****
-
-// import 'package:flutter/material.dart';
-// import 'package:tlydp/reusables/navbar/nav.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: "Bottom Nav Bar",
-//       home: Nav(),
-
-//     );
-//   }
-// }
