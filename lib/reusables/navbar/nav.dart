1f3124817694e72a1a256fb7fa203ea7ec1293bd
Alina Ananjeva <alina.ananjeva@eurodrivefinance.co.uk>
2022-01-28 17:52:39 +0000

    working on filter map feature

--- lib/reusables/navbar/nav.dart
+++ lib/reusables/navbar/nav.dart
@@ -8,73 +8,69 @@ class Nav extends StatefulWidget {
 }
 
 class _NavState extends State<Nav> {
-
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            child: IconButton(
-              iconSize: 70,
-              onPressed: () { 
-                // Go to map page
-              }, 
-              icon: Image.asset("assets/images/map-icon.png"),
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 10,
-                  offset: const Offset(1, 1),
-                )
-              ]
-            )
-          ),
-          Container(
-            child: IconButton(
-              iconSize: 70,
-              onPressed: () {
-                // Go to profile page
-              }, 
-              icon: Image.asset("assets/images/profile-icon.png"),
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 10,
-                  offset: const Offset(1, 1),
-                )
-              ]
-            )
-          ),
-          Container(
-            child: IconButton(
-              iconSize: 70,
-              onPressed: () {
-                // Go to about us page
-              }, 
-              icon: Image.asset("assets/images/aboutus-icon.png"),
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 10,
-                  offset: const Offset(1, 1),
-                )
-              ]
-            )
-          ),
-        ],
-      )
+    return Container(
+      height: 65,
+      child: Align(
+          alignment: Alignment.bottomCenter,
+          heightFactor: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                  child: IconButton(
+                    iconSize: 70,
+                    onPressed: () {
+                      // Go to map page
+                    },
+                    icon: Image.asset("assets/images/map-icon.png"),
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: const Offset(1, 1),
+                        )
+                      ])),
+              Container(
+                  child: IconButton(
+                    iconSize: 70,
+                    onPressed: () {
+                      // Go to profile page
+                    },
+                    icon: Image.asset("assets/images/profile-icon.png"),
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: const Offset(1, 1),
+                        )
+                      ])),
+              Container(
+                  child: IconButton(
+                    iconSize: 70,
+                    onPressed: () {
+                      // Go to about us page
+                    },
+                    icon: Image.asset("assets/images/aboutus-icon.png"),
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: const Offset(1, 1),
+                        )
+                      ])),
+            ],
+          )),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_collection_literals, avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
-
+import '../reusables/navbar/nav.dart';
 import '../shared/menu_drawer.dart';
+import '../shared/map.dart';
+import '../widgets/map_radio_buttons.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,22 +13,36 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Image(
-                image: AssetImage("assets/images/yellow-outlined-duck.png"),
-              ),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-            );
-          },
-        ),
-      ),
+          backgroundColor: Color.fromARGB(255, 223, 105, 8),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Image(
+                  image: AssetImage("assets/images/yellow-outlined-duck.png"),
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+          title: Text('Home Page',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white))),
       drawer: MenuDrawer(),
+      body: Container(
+          child: Column(
+        mainAxisSize: MainAxisSize.max,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          Container(height: 100, child: MapRadioButton()),
+          Expanded(child: Map()),
+        ],
+      )),
+      bottomNavigationBar: Nav(),
     );
   }
 }
